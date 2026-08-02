3f971aa65712aa2e940fa2bac33c83212a001f07
md-haseb <rahman.mdhaseb@gmail.com>
2026-08-03 01:10:36 +0600

    wip(content): implement hide shorts feature

--- content/content.js
+++ content/content.js
@@ -1,4 +1,38 @@
 chrome.runtime.onMessage.addListener((message) => {
   console.log(message.type);
   console.log(message.enabled);
+
+  if (message.type !== "hideShorts") return;
+
+  const elements = document.querySelectorAll(
+    "ytd-rich-shelf-renderer[is-shorts]"
+  );
+
+  elements.forEach((element) => {
+    const sectionParent = element.closest("ytd-rich-section-renderer");
+
+    if (sectionParent) {
+      sectionParent.style.display = message.enabled ? "none" : "";
+    } else {
+      const gridParent = element.closest("grid-shelf-view-model");
+      const host = gridParent?.querySelector(".ytGridShelfViewModelHost");
+
+      if (host) {
+        host.style.display = message.enabled ? "none" : "";
+      }
+    }
+
+    // const parentElm =
+    //   element.closest("ytd-rich-section-renderer") ||
+    //   element.closest("grid-shelf-view-model");
+
+    // if (!parentElm) return;
+
+    // parentElm.style.display = message.enabled ? "none" : "";
+  });
+  
 });
+
+
+
+
